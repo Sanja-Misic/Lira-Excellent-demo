7b5b99cf8b1ca3d797852883b00cbe07295a6344
Sanja Misic <sanjamisic98@gmail.com>
2023-06-04 18:50:26 +0200

    Fixed issue with DOM manipulation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // PRELOADER
 const preloader = document.querySelector('.preloader');
 
@@ -8,65 +9,67 @@ const hideloader = () => {
 
 window.addEventListener('load', hideloader);
 
-//ACCOMODATION FUNCIONALITY
-// const doubleRoomBtn = document.querySelector('.double-room');
-// const tripleRoomBtn = document.querySelector('.triple-room');
-// const fourRoomBtn = document.querySelector('.four-bed-room');
-// const fiveRoomBtn = document.querySelector('.five-bed-room');
-// const studioRoomBtn = document.querySelector('.studio-room');
+// ACCOMODATION FUNCIONALITY
+const doubleRoomBtn = document.querySelector('.double-room');
+const tripleRoomBtn = document.querySelector('.triple-room');
+const fourRoomBtn = document.querySelector('.four-bed-room');
+const fiveRoomBtn = document.querySelector('.five-bed-room');
+const studioRoomBtn = document.querySelector('.studio-room');
 
-// const accomodationContainer = document.querySelector('.accomodation-about');
-// const doubleRoomContainer = document.querySelector('.double-room-container');
-// const tripleRoomContainer = document.querySelector('.triple-room-container');
-// const fourRoomContainer = document.querySelector('.four-bed-room-container');
-// const fiveRoomContainer = document.querySelector('.five-bed-room-container');
-// const studioRoomContainer = document.querySelector('.accomodation-studio');
+const accomodationContainer = document.querySelector('.accomodation-about');
+const doubleRoomContainer = document.querySelector('.double-room-container');
+const tripleRoomContainer = document.querySelector('.triple-room-container');
+const fourRoomContainer = document.querySelector('.four-bed-room-container');
+const fiveRoomContainer = document.querySelector('.five-bed-room-container');
+const studioRoomContainer = document.querySelector('.accomodation-studio');
 
-// const roomBtns = [
-//   doubleRoomBtn,
-//   tripleRoomBtn,
-//   fourRoomBtn,
-//   fiveRoomBtn,
-//   studioRoomBtn,
-// ];
+const roomBtns = [
+  doubleRoomBtn,
+  tripleRoomBtn,
+  fourRoomBtn,
+  fiveRoomBtn,
+  studioRoomBtn,
+];
 
-// const roomContainers = [
-//   doubleRoomContainer,
-//   tripleRoomContainer,
-//   fourRoomContainer,
-//   fiveRoomContainer,
-//   studioRoomContainer,
-// ];
+const roomContainers = [
+  doubleRoomContainer,
+  tripleRoomContainer,
+  fourRoomContainer,
+  fiveRoomContainer,
+  studioRoomContainer,
+];
 
-// roomBtns.forEach(btn => {
-//   btn.addEventListener('click', function () {
-//     console.log('radi');
-//     accomodationContainer.classList.add('accomodation-about-hidden');
+roomBtns.forEach(btn => {
+  if (btn !== null) {
+    btn.addEventListener('click', function () {
+      console.log('radi');
+      accomodationContainer.classList.add('accomodation-about-hidden');
 
-//     roomContainers.forEach(container => {
-//       console.log(container);
-//       container.classList.remove('rooms-show');
-//     });
+      roomContainers.forEach(container => {
+        console.log(container);
+        container.classList.remove('rooms-show');
+      });
 
-//     switch (btn) {
-//       case doubleRoomBtn:
-//         doubleRoomContainer.classList.add('rooms-show');
-//         break;
-//       case tripleRoomBtn:
-//         tripleRoomContainer.classList.add('rooms-show');
-//         break;
-//       case fourRoomBtn:
-//         fourRoomContainer.classList.add('rooms-show');
-//         break;
-//       case fiveRoomBtn:
-//         fiveRoomContainer.classList.add('rooms-show');
-//         break;
-//       case studioRoomBtn:
-//         studioRoomContainer.classList.add('accomodation-studio-show');
-//         break;
-//     }
-//   });
-// });
+      switch (btn) {
+        case doubleRoomBtn:
+          doubleRoomContainer.classList.add('rooms-show');
+          break;
+        case tripleRoomBtn:
+          tripleRoomContainer.classList.add('rooms-show');
+          break;
+        case fourRoomBtn:
+          fourRoomContainer.classList.add('rooms-show');
+          break;
+        case fiveRoomBtn:
+          fiveRoomContainer.classList.add('rooms-show');
+          break;
+        case studioRoomBtn:
+          studioRoomContainer.classList.add('accomodation-studio-show');
+          break;
+      }
+    });
+  }
+});
 
 // BACK TO TOP BUTTON
 const backToTopBtn = document.querySelector('.back-to-top-btn');
